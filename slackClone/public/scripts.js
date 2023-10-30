cb594f8b07bee292b30ef6f7c4c5b8a749f5c53a
Alex <k8cchaha@gmail.com>
2023-10-30 11:25:10 +0800

    add room render

--- slackClone/public/scripts.js
+++ slackClone/public/scripts.js
@@ -2,8 +2,6 @@ console.log('Client Start')
 const userName = prompt('Whats your name?')
 const userPW = prompt('Whats your password?')
 
-
-
 const socket = io('http://localhost:9001')
 
 socket.on('connect', ()=>{
@@ -16,6 +14,24 @@ socket.on('nsList', (data)=>{
   const namespacesDiv = document.querySelector('.namespaces')
   data.forEach(ns=>{
     // updated the HTML
-    namespacesDiv.innerHTML += `<div class="namespace" ns="${ns.name}"><img src="${ns.image}"></div>`
+    namespacesDiv.innerHTML += `<div class="namespace" ns="${ns.endpoint}"><img src="${ns.image}"></div>`
+  })
+
+  Array.from(document.getElementsByClassName('namespace')).forEach((elem)=>{
+    console.log(elem)
+    elem.addEventListener('click', e => {
+      const nsEndpoint = elem.getAttribute('ns')
+      console.log(nsEndpoint)
+
+      const clickdNs = data.find(row=>row.endpoint === nsEndpoint)
+      const rooms = clickdNs.rooms
+
+      let roomList = document.querySelector('.room-list')
+      roomList.innerHTML = ''
+      rooms.forEach(room=>{
+        roomList.innerHTML += `<li><span class="glyphicon glyphicon-lock"></span>${room.roomTitle}</li>`
+      })
+    })
+
   })
 })
